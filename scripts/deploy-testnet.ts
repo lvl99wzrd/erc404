a5c312f355bc1c599b0ef327bf461310916c3c27
marcoelissa <marco.elissa@gmail.com>
2024-02-12 12:46:18 +0700

    Update contract deployment parameters

--- scripts/deploy-testnet.ts
+++ scripts/deploy-testnet.ts
@@ -5,11 +5,11 @@ async function main() {
   const owner = vars.get("ARNF_TEST_WALLET_ADDRESS")
   const factory = await ethers.getContractFactory("ArtNotFound")
   const contract = await factory.deploy(
-    "Art Not Found",
-    "ARNF",
-    "ipfs://bafybeicl52gqhj7ksagces7yfmxodc7j34crip4umy2uavl7ojkrkfqiee/",
-    404n,
-    18n,
+    "404",
+    "TANF",
+    "https://bafybeiefrqmoh2dbtrsw53w2htycajq4aeg5xc72paoxlefqgq6zk23cna.ipfs.nftstorage.link/",
+    18,
+    404,
     owner,
     owner,
   )
@@ -17,7 +17,7 @@ async function main() {
   await contract.waitForDeployment()
   const contractAddress = await contract.getAddress()
 
-  console.log("ArtNotFound deployed to:", contractAddress)
+  console.log("Contract deployed to:", contractAddress)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
